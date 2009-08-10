a30f3a255ddeff3e6d64f8b2740aca5c46f110ce
sem <sem_xxx@ukr.net>
2009-08-10 18:57:29 +0300

    Installed site design

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $this->_redirect('/forum');
+      // $this->_redirect('/forum');
     }
 
 
